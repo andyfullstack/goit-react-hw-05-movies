a97115fd08fe16caae868d2719a62704db2c8498
andyfullstack <135138778+andyfullstack@users.noreply.github.com>
2024-01-15 18:12:35 +0000

    Deploying to gh-pages from @ andyfullstack/goit-react-hw-05-movies@d42153a7fa442e14f76f5cb31038f08411601b63 🚀

--- static/js/216.8d4e7c33.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[216],{216:(e,t,a)=>{a.r(t),a.d(t,{NotFound:()=>o});var r=a(184);const o=()=>(0,r.jsx)("h1",{style:{color:"red",fontStyle:"500"},children:"What'are folk men! Please try again!"})}}]);
-//# sourceMappingURL=216.8d4e7c33.chunk.js.map
